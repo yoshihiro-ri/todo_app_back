3cd0011e27a6a89ea38ba95022d8d1d027f190e0
yoshihiro-ri <ri.dev1027@gmail.com>
2023-11-13 20:17:02 +0900

    テーブル単位で区切ってroutesに入れる

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import todo_app
 todo_app.dont_write_bytecode = True
 
 from todo_app import app
-from todo_app import routes
-#routes内のblueprintを登録する
-app.register_blueprint(routes.user_bp)
+#routes/user_routes.pyをインポート
+from todo_app.routes import user_routes
+#routes/usr_route.py内のblueprintをappに登録
+app.register_blueprint(user_routes.user_bp)
 if __name__ == '__main__':
     app.run(debug=True)
