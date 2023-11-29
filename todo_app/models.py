9349efad032198943411fffc19a8400dc9b50a3b
yoshihiro-ri <ri.dev1027@gmail.com>
2023-11-29 13:22:03 +0900

    TaskCardのCRUDをフロント繋げた

--- todo_app/models.py
+++ todo_app/models.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template, request, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, desc
 from flask_login import UserMixin
+from flask_migrate import Migrate
 
 class User(UserMixin,db.Model):
     __tablename__ = 'User'
@@ -17,17 +18,19 @@ class User(UserMixin,db.Model):
 class TaskCard(db.Model):
     __tablename__ = 'TaskCard'
     id = db.Column(Integer, primary_key=True) 
-    user_id = db.Column(Integer, db.ForeignKey('User.id'), nullable=False)
-    title = db.Column(String(32),nullable=False)
+    user_id = db.Column(Integer, db.ForeignKey('User.id'))
+    card_id = db.Column(String(36))
+    title = db.Column(String(32))
     tasks = db.relationship('Task', backref='task_card', cascade="all,delete")
 
 class Task(db.Model):
     __tablename__ = 'Task'
-    id = db.Column(Integer, primary_key=True) 
-    content = db.Column(String(32),nullable=False)
+    id = db.Column(Integer, primary_key=True)
+    task_id = db.Column(String(36))
+    content = db.Column(String(32))
     status = db.Column(db.Integer, nullable=False, default=0)
-    task_card_id = db.Column(Integer, db.ForeignKey('TaskCard.id'), nullable=False)
-
+    task_card_id = db.Column(Integer, db.ForeignKey('TaskCard.id'))
+    
 def init():
     with app.app_context():
         db.create_all()
@@ -36,4 +39,4 @@ def init():
         # test_db.init()
 
 init()
-
+migrate = Migrate(app, db)
