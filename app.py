82b08d1062033a12d647918f175f5fd75e6e0c7e
yoshihiro-ri <ri.dev1027@gmail.com>
2023-11-30 16:25:17 +0900

    task周り完成

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import todo_app
 from todo_app import app
 from todo_app.routes import user_routes
+from todo_app.routes import task_routes
 from todo_app.routes import task_card_routes
 from todo_app.routes import auth
 from flask_login import LoginManager
@@ -11,12 +12,18 @@ login_manager.init_app(app)
 todo_app.dont_write_bytecode = True
 app.register_blueprint(user_routes.user_bp)
 app.register_blueprint(task_card_routes.task_card_bp)
+app.register_blueprint(task_routes.task_bp)
+
 app.register_blueprint(auth.auth_bp)
 app.config.update(
     SESSION_COOKIE_SECURE=True,
     SESSION_COOKIE_SAMESITE='None'
 )
 CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}}, methods=['GET', 'POST', 'PUT', 'DELETE'], allow_headers=['Content-Type', 'Authorization'], supports_credentials=True)
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-store"
+    return response
 @login_manager.user_loader
 def load_user(user_id):
     from todo_app.models import User
--- todo_app/routes/task_routes.py
+++ todo_app/routes/task_routes.py
@@ -9,7 +9,7 @@ task_bp= Blueprint('task', __name__, url_prefix='/task')
 class TaskSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Task
-        fields = ("id", "user_id", "title")  
+        fields = ("id", "task_card_id", "task_id","content","order_index")
 task_schema = TaskSchema(many=True)
 
 @task_bp.route('/', methods=['GET'])
@@ -17,37 +17,43 @@ def get_all_tasks():
     data = Task.query.all()
     return jsonify(task_schema.dump(data))
 
-@task_bp.route('/<int:id>', methods=["GET"])
-def get_task(id):
-    data = Task.query.filter_by(id=id).all() 
+@task_bp.route('/<uuid>', methods=['GET'])
+def get_all_tasks_by_card_id(uuid):
+    data = Task.query.filter_by(task_card_id=uuid).order_by(Task.order_index).all()
     return jsonify(task_schema.dump(data))
+
+# @task_bp.route('/<int:id>', methods=["GET"])
+# def get_task(id):
+#     data = Task.query.filter_by(id=id).all() 
+#     return jsonify(task_schema.dump(data))
 
 @task_bp.route('/', methods=['POST'])
 def create_task():
     entry = Task()
     data = get_data_from_json(request)
 
-    entry.user_id = data["user_id"]
-    entry.title = data["title"]
+    entry.task_id = data["task_id"]
+    entry.task_card_id = data["task_card_id"]
+    entry.content = data["content"]
+    
     add_entry_and_close_session(entry)
 
     latestData = Task.query.order_by(desc(Task.id)).first()   
     return redirect('/task/' + str(latestData.id))
 
-@task_bp.route('/<int:id>', methods=['PUT'])
-def update_task(id):
-    entry = Task.query.get(id)
+@task_bp.route('/<uuid>', methods=['PUT'])
+def update_task(uuid):
+    entry = Task.query.filter_by(task_id=uuid).first()
     data = get_data_from_json(request)
+    entry.order_index = data['order_index']
     
-    entry.user_id = data["user_id"]
-    entry.title = data["title"]
     add_entry_and_close_session(entry)
 
     return redirect('/task/' + str(id))
 
-@task_bp.route('/<int:id>', methods=['DELETE'])
-def delete_task(id):
-    entry = Task.query.get(id)
+@task_bp.route('/<uuid>', methods=['DELETE'])
+def delete_task(uuid):
+    entry = Task.query.filter_by(task_id=uuid).first()
     delete_entry_and_close_session(entry)
 
     return jsonify({'message': 'Task card deleted successfully'})
